04cc22c60758de08c3433f00675d3b5495c63ca4
Adittt11 <146904866+Adittt11@users.noreply.github.com>
2024-06-20 14:12:19 +0800

    Update CustomerHandler.java

--- CustomerHandler.java
+++ CustomerHandler.java
@@ -1,208 +1,203 @@
-package handlers;
-
-import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import utils.DatabaseUtil;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONObject;
 
-public class CustomerHandler extends BaseHandler {
-
+public class CustomerHandler implements HttpHandler {
     @Override
-    protected void handleRequest(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange exchange) throws IOException {
+        if (!Main.isAuthenticated(exchange.getRequestHeaders().getFirst("API-Key"))) {
+            Utils.sendResponse(exchange, "Forbidden", 403);
+            return;
+        }
+
         String method = exchange.getRequestMethod();
+        String response = "";
+        int responseCode = 200;
 
-        switch (method) {
-            case "GET":
-                handleGet(exchange);
-                break;
-            case "POST":
-                handlePost(exchange);
-                break;
-            case "PUT":
-                handlePut(exchange);
-                break;
-            case "DELETE":
-                handleDelete(exchange);
-                break;
-            default:
-                sendResponse(exchange, 405, "Method Not Allowed");
-        }
-    }
-
-    private void handleGet(HttpExchange exchange) throws IOException {
-        String path = exchange.getRequestURI().getPath();
-        String[] segments = path.split("/");
-        try (Connection conn = connect()) {
-            if (segments.length == 2) {
-                // GET /customers
-                String query = "SELECT * FROM customers";
-                PreparedStatement stmt = conn.prepareStatement(query);
-                ResultSet rs = stmt.executeQuery();
-                JSONArray customers = new JSONArray();
-                while (rs.next()) {
-                    JSONObject customer = new JSONObject();
-                    customer.put("id", rs.getInt("id"));
-                    customer.put("email", rs.getString("email"));
-                    customer.put("first_name", rs.getString("first_name"));
-                    customer.put("last_name", rs.getString("last_name"));
-                    customer.put("phone_number", rs.getString("phone_number"));
-                    customers.put(customer);
-                }
-                sendResponse(exchange, 200, customers.toString());
-            } else if (segments.length == 3) {
-                int customerId = Integer.parseInt(segments[2]);
-                // GET /customers/{id}
-                String query = "SELECT * FROM customers WHERE id = ?";
-                PreparedStatement stmt = conn.prepareStatement(query);
-                stmt.setInt(1, customerId);
-                ResultSet rs = stmt.executeQuery();
-                if (rs.next()) {
-                    JSONObject customer = new JSONObject();
-                    customer.put("id", rs.getInt("id"));
-                    customer.put("email", rs.getString("email"));
-                    customer.put("first_name", rs.getString("first_name"));
-                    customer.put("last_name", rs.getString("last_name"));
-                    customer.put("phone_number", rs.getString("phone_number"));
-
-                    // Get addresses
-                    String addressQuery = "SELECT * FROM shipping_addresses WHERE customer = ?";
-                    PreparedStatement addrStmt = conn.prepareStatement(addressQuery);
-                    addrStmt.setInt(1, customerId);
-                    ResultSet addrRs = addrStmt.executeQuery();
-                    JSONArray addresses = new JSONArray();
-                    while (addrRs.next()) {
-                        JSONObject address = new JSONObject();
-                        address.put("id", addrRs.getInt("id"));
-                        address.put("title", addrRs.getString("title"));
-                        address.put("line1", addrRs.getString("line1"));
-                        address.put("line2", addrRs.getString("line2"));
-                        address.put("city", addrRs.getString("city"));
-                        address.put("province", addrRs.getString("province"));
-                        address.put("postcode", addrRs.getString("postcode"));
-                        addresses.put(address);
+        try {
+            switch (method) {
+                case "GET":
+                    String[] uriParts = exchange.getRequestURI().toString().split("/");
+                    if (uriParts.length == 2) {
+                        response = getAllCustomers();
+                    } else if (uriParts.length == 3) {
+                        int id = Integer.parseInt(uriParts[2]);
+                        response = getCustomerById(id);
+                    } else if (uriParts.length == 4 && uriParts[3].equals("cards")) {
+                        int id = Integer.parseInt(uriParts[2]);
+                        response = getCustomerCards(id);
+                    } else if (uriParts.length == 4 && uriParts[3].equals("subscriptions")) {
+                        int id = Integer.parseInt(uriParts[2]);
+                        response = getCustomerSubscriptions(id, null);
+                    } else if (uriParts.length == 5 && uriParts[3].equals("subscriptions")) {
+                        int id = Integer.parseInt(uriParts[2]);
+                        String status = uriParts[4];
+                        response = getCustomerSubscriptions(id, status);
                     }
-                    customer.put("addresses", addresses);
-                    sendResponse(exchange, 200, customer.toString());
-                } else {
-                    sendResponse(exchange, 404, "Customer not found");
-                }
+                    break;
+                case "POST":
+                    String requestBody = Utils.getRequestBody(exchange);
+                    response = createCustomer(requestBody);
+                    responseCode = 201;
+                    break;
+                case "PUT":
+                    // Handle PUT request
+                    break;
+                case "DELETE":
+                    // Handle DELETE request
+                    break;
+                default:
+                    responseCode = 405;
+                    response = "Method Not Allowed";
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
+            responseCode = 500;
+            response = "Internal Server Error";
             e.printStackTrace();
-            sendResponse(exchange, 500, "Internal Server Error");
         }
+
+        Utils.sendResponse(exchange, response, responseCode);
     }
 
-    private void handlePost(HttpExchange exchange) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
-        StringBuilder requestBody = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            requestBody.append(line);
-        }
+    private String getAllCustomers() {
+        List<String> customers = new ArrayList<>();
+        try (Connection conn = Database.connect();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM customers")) {
+            ResultSet rs = pstmt.executeQuery();
 
-        JSONObject customerJson = new JSONObject(requestBody.toString());
-
-        try (Connection conn = connect()) {
-            String query = "INSERT INTO customers (email, first_name, last_name, phone_number) VALUES (?, ?, ?, ?)";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, customerJson.getString("email"));
-            stmt.setString(2, customerJson.getString("first_name"));
-            stmt.setString(3, customerJson.getString("last_name"));
-            stmt.setString(4, customerJson.getString("phone_number"));
-            int rowsAffected = stmt.executeUpdate();
-
-            if (rowsAffected > 0) {
-                sendResponse(exchange, 201, "Customer created successfully");
-            } else {
-                sendResponse(exchange, 400, "Failed to create customer");
+            while (rs.next()) {
+                String customer = String.format(
+                        "{\"id\": %d, \"email\": \"%s\", \"first_name\": \"%s\", \"last_name\": \"%s\", \"phone_number\": \"%s\"}",
+                        rs.getInt("id"),
+                        rs.getString("email"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("phone_number"));
+                customers.add(customer);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            sendResponse(exchange, 500, "Internal Server Error");
         }
+        return "[" + String.join(",", customers) + "]";
     }
 
-    private void handlePut(HttpExchange exchange) throws IOException {
-        String path = exchange.getRequestURI().getPath();
-        String[] segments = path.split("/");
-        if (segments.length != 3) {
-            sendResponse(exchange, 400, "Invalid URL");
-            return;
-        }
+    private String getCustomerById(int id) {
+        String response = "";
+        try (Connection conn = Database.connect();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM customers WHERE id = ?")) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
 
-        int customerId = Integer.parseInt(segments[2]);
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
-        StringBuilder requestBody = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            requestBody.append(line);
-        }
-
-        JSONObject customerJson = new JSONObject(requestBody.toString());
-
-        try (Connection conn = connect()) {
-            String query = "UPDATE customers SET email = ?, first_name = ?, last_name = ?, phone_number = ? WHERE id = ?";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, customerJson.getString("email"));
-            stmt.setString(2, customerJson.getString("first_name"));
-            stmt.setString(3, customerJson.getString("last_name"));
-            stmt.setString(4, customerJson.getString("phone_number"));
-            stmt.setInt(5, customerId);
-            int rowsAffected = stmt.executeUpdate();
-
-            if (rowsAffected > 0) {
-                sendResponse(exchange, 200, "Customer updated successfully");
+            if (rs.next()) {
+                response = String.format(
+                        "{\"id\": %d, \"email\": \"%s\", \"first_name\": \"%s\", \"last_name\": \"%s\", \"phone_number\": \"%s\"}",
+                        rs.getInt("id"),
+                        rs.getString("email"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("phone_number"));
             } else {
-                sendResponse(exchange, 404, "Customer not found");
+                response = "404 - Not Found";
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            sendResponse(exchange, 500, "Internal Server Error");
+            response = "404 - Not Found";
         }
+        return response;
     }
 
-    private void handleDelete(HttpExchange exchange) throws IOException {
-        String path = exchange.getRequestURI().getPath();
-        String[] segments = path.split("/");
-        if (segments.length != 3) {
-            sendResponse(exchange, 400, "Invalid URL");
-            return;
-        }
+    private String getCustomerCards(int customerId) {
+        List<String> cards = new ArrayList<>();
+        try (Connection conn = Database.connect();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM cards WHERE customer = ?")) {
+            pstmt.setInt(1, customerId);
+            ResultSet rs = pstmt.executeQuery();
 
-        int customerId = Integer.parseInt(segments[2]);
-
-        try (Connection conn = connect()) {
-            String query = "DELETE FROM customers WHERE id = ?";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setInt(1, customerId);
-            int rowsAffected = stmt.executeUpdate();
-
-            if (rowsAffected > 0) {
-                sendResponse(exchange, 200, "Customer deleted successfully");
-            } else {
-                sendResponse(exchange, 404, "Customer not found");
+            while (rs.next()) {
+                String card = String.format(
+                        "{\"id\": %d, \"customer\": %d, \"card_type\": \"%s\", \"masked_number\": \"%s\", \"expiry_month\": %d, \"expiry_year\": %d, \"status\": \"%s\", \"is_primary\": %d}",
+                        rs.getInt("id"),
+                        rs.getInt("customer"),
+                        rs.getString("card_type"),
+                        rs.getString("masked_number"),
+                        rs.getInt("expiry_month"),
+                        rs.getInt("expiry_year"),
+                        rs.getString("status"),
+                        rs.getInt("is_primary"));
+                cards.add(card);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            sendResponse(exchange, 500, "Internal Server Error");
-
-            //work with json library
-            Gson gson = new Gson();
-
         }
+        return "[" + String.join(",", cards) + "]";
+    }
+
+    private String getCustomerSubscriptions(int customerId, String status) {
+        List<String> subscriptions = new ArrayList<>();
+        String query = "SELECT * FROM subscriptions WHERE customer = ?";
+        if (status != null) {
+            query += " AND status = ?";
+        }
+
+        try (Connection conn = Database.connect();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, customerId);
+            if (status != null) {
+                pstmt.setString(2, status);
+            }
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String subscription = String.format(
+                        "{\"id\": %d, \"customer\": %d, \"billing_period\": %d, \"billing_period_unit\": \"%s\", \"total_due\": %d, \"activated_at\": \"%s\", \"current_term_start\": \"%s\", \"current_term_end\": \"%s\", \"status\": \"%s\"}",
+                        rs.getInt("id"),
+                        rs.getInt("customer"),
+                        rs.getInt("billing_period"),
+                        rs.getString("billing_period_unit"),
+                        rs.getInt("total_due"),
+                        rs.getString("activated_at"),
+                        rs.getString("current_term_start"),
+                        rs.getString("current_term_end"),
+                        rs.getString("status"));
+                subscriptions.add(subscription);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "[" + String.join(",", subscriptions) + "]";
+    }
+
+    private String createCustomer(String requestBody) {
+        JSONObject json = Utils.parseJsonBody(requestBody);
+        if (!json.has("email") || !json.has("first_name") || !json.has("last_name") || !json.has("phone_number")) {
+            return "400 - Bad Request: Missing required fields";
+        }
+
+        String email = json.getString("email");
+        String firstName = json.getString("first_name");
+        String lastName = json.getString("last_name");
+        String phoneNumber = json.getString("phone_number");
+
+        try (Connection conn = Database.connect();
+             PreparedStatement pstmt = conn.prepareStatement(
+                     "INSERT INTO customers (email, first_name, last_name, phone_number) VALUES (?, ?, ?, ?)")) {
+            pstmt.setString(1, email);
+            pstmt.setString(2, firstName);
+            pstmt.setString(3, lastName);
+            pstmt.setString(4, phoneNumber);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "500 - Internal Server Error";
+        }
+
+        return "201 - Created";
     }
 }
-
-//
-
